505344e5bb06a8ffd44fba95362ce4269ac4ca5c
SK-Coder <soluk224@gmail.com>
2026-04-27 17:10:45 +0530

    env understanding

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const dbgr = require('debug') ("development:mongoose");
 
 mongoose.connect("mongodb+srv://soluCoder23:Solu2004%40%23@sk-userdata.uzljo4c.mongodb.net/?appName=SK-UserData")
     .then(function () {
