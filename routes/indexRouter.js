adc697ba2efcc083d0642ab6a772e63cf5d398d3
SK-Coder <soluk224@gmail.com>
2026-05-21 16:54:05 +0530

    account updated

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router()
 const isLoggedIn = require('../middleware/is.loggedIn')
 const userModel = require('../models/user.model')
-const productModel = require('../models/product.model')
+const productModel = require('../models/product.model');
+const upload = require('../config/multer.config');
 
 // routers
 router.get('/', (req, res) => {
@@ -36,6 +37,33 @@ router.get('/myaccount', isLoggedIn, async (req, res) => {
     }
 })
 
+// edit product
+router.get('/myaccount/edit/:id', isLoggedIn, async (req, res) => {
+    try {
+        const { id } = req.params;
+        let product = await productModel.findById(id)
+        res.render('editProduct', { product })
+    } catch (error) {
+        req.flash('error', 'Error fetching product details');
+        res.status(500).send('Error fetching product details');
+    }
+});
+
+router.post('/myaccount/edit', isLoggedIn, upload.single('image'), async (req, res) => {
+    try {
+        const { id, name, price, discount } = req.body;
+        const image = req.file.buffer;
+        await productModel.findByIdAndUpdate(id, { name, price, discount, image });
+
+        req.flash('success', 'Product updated successfully');
+        res.redirect('/myaccount');
+    } catch (error) {
+        req.flash('error', 'Error updating product');
+        res.status(500).send('Error updating product');
+    }
+});
+
+
 // delete product
 router.get("/myaccount/delete/:id", isLoggedIn, async (req, res)=>{
     try {
@@ -44,7 +72,7 @@ router.get("/myaccount/delete/:id", isLoggedIn, async (req, res)=>{
         await productModel.findByIdAndDelete(id);
 
         req.flash('success', 'Product deleted successfully');
-        res.redirect('/shop/products');
+        res.redirect('/myaccount');
     } catch (error) {
         req.flash('error', 'Error deleting product');
         res.status(500).send('Error deleting product');
