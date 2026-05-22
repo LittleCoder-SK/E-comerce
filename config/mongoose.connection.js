5855caa43e5454f4f4d66b4b1b7d728ca288687b
SK-Coder <soluk224@gmail.com>
2026-05-22 09:40:05 +0530

    mongodb uri update

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('config')
-// const dbgr = require('debug') ("development:mongoose");
 
-mongoose.connect(`${config.get("MONGODB_URI")}/${config.get("DB_NAME")}`)
+// mongoose.connect(`${config.get("MONGODB_URI")}/${config.get("DB_NAME")}`)
+mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
     .then(function () {
         console.log('db connected');
     })
